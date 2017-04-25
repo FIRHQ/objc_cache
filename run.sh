7932ef9b6d8e0546658b2a993026c18b881f411f
Mayi <my3157@hotmail.com>
2017-04-25 16:07:09 +0800

    update to /cache

--- run.sh
+++ run.sh
@@ -1,23 +1,17 @@
 cd $FLOW_CURRENT_PROJECT_PATH
 
 if [[ $FLOW_ENABLE_CACHE != 'FALSE' ]]; then
-ftp -n -i >> ftp.log <<EOF
-open 172.16.152.110
-user flow flow.ci
-cd flow
-cd ${FLOW_USER_ID}/${FLOW_PROJECT_ID}
-get Pods.tar.gz
-bye
-EOF
-
-if [ -f Pods.tar.gz ];then
-    tar -xf Pods.tar.gz
-    echo "cache exists"
+    echo 'start objc cache'
+    FLOW_CACHE_PATH="$CACHE_BASE_URL/Pods"
+    if [[ -d $FLOW_CACHE_PATH ]]; then
+        echo "cache exists"
+    else
+        echo "cache not exists"
+        mkdir -p $FLOW_CACHE_PATH
+    fi
+    echo "$(date)" > ${FLOW_CACHE_PATH}/version
+    rm -rf $FLOW_CURRENT_PROJECT_PATH/Pods
+    ln -s $FLOW_CACHE_PATH $FLOW_CURRENT_PROJECT_PATH/
 else
-    mkdir -pv Pods
-    echo "$(date)" > Pods/version
-    echo "cache not exists"
-fi
-else
-  echo 'cache disabled'
+    echo 'cache disabled'
 fi
